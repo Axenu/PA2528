ca45fadfd898acb8612accee03b068cba9f730e2
Mikael Hernvall <mikael.hernvall@gmail.com>
2017-11-11 12:28:15 +0100

    Finalize StompAllocator implementation

--- StompAllocator.cpp
+++ StompAllocator.cpp
@@ -3,30 +3,62 @@
 #include <iostream>
 #include <cstdlib>
 
+#ifdef __WIN32
+#include <malloc.h>
 #include <windows.h>
-
-StompAllocator::StompAllocator() {
-    std::cout << "ctor StompAllocator" << std::endl;
-
+const size_t StompAllocator::M_PAGE_SIZE = []() {
     SYSTEM_INFO sysInfo;
     GetSystemInfo(&sysInfo);
+    return sysInfo.dwPageSize;
+}();
+#define aligned_malloc(size, alignment) _aligned_malloc(size, alignment)
+#define aligned_free(memblock) _aligned_free(memblock)
+#define protect(block, size, access, previousAccess) VirtualProtect(block, size, access, previousAccess)
+#else
+#error Unsupported platform.
+#endif
 
-    std::cout << sysInfo.dwPageSize << std::endl;
-    for(size_t i = 0; i < 10; i++)
-    {
-        char* a = new char[sysInfo.dwPageSize - 8];
-        std::cout << ((size_t)a) % sysInfo.dwPageSize << std::endl;
-    }
-
+StompAllocator::StompAllocator(bool checkOverrun)
+: M_CHECK_OVERRUN(checkOverrun) {
 }
+
 StompAllocator::~StompAllocator() {
-    std::cout << "dtor StompAllocator" << std::endl;
 }
 
 void* StompAllocator::alloc_internal(size_t size) {
-    return malloc(size);
+    if(M_CHECK_OVERRUN) {
+        size += sizeof(BlockSize);
+    }
+
+    size_t distanceToPageBoundary = (M_PAGE_SIZE - size % M_PAGE_SIZE) % M_PAGE_SIZE;
+    BlockSize blockSize = size + M_PAGE_SIZE + distanceToPageBoundary;
+    char* block = (char*)aligned_malloc(blockSize, M_PAGE_SIZE);
+
+    DWORD prevProtect;
+    if(M_CHECK_OVERRUN) {
+        protect(block + blockSize - M_PAGE_SIZE, M_PAGE_SIZE, PAGE_NOACCESS, &prevProtect);
+        char* ptr = block + distanceToPageBoundary;
+        *((BlockSize*)ptr) = blockSize;
+        return ptr + sizeof(BlockSize);
+    }
+    else {
+        protect(block, M_PAGE_SIZE, PAGE_NOACCESS, &prevProtect);
+        return block + M_PAGE_SIZE;
+    }
 }
 
 void StompAllocator::dealloc_internal(void *p) {
-    free(p);
+    DWORD prevProtect;
+    char* block;
+    if(M_CHECK_OVERRUN) {
+        char* ptr = (char*)p - sizeof(BlockSize);
+        block = ptr - size_t(ptr) % M_PAGE_SIZE;
+        BlockSize blockSize = *((BlockSize*)ptr);
+        protect(block + blockSize - M_PAGE_SIZE, M_PAGE_SIZE, PAGE_READWRITE, &prevProtect);
+    }
+    else {
+        block = (char*)p - M_PAGE_SIZE;
+        protect(block, M_PAGE_SIZE, PAGE_READWRITE, &prevProtect);
+    }
+    aligned_free(block);
 }
--- StompAllocator.h
+++ StompAllocator.h
@@ -5,12 +5,18 @@
 
 class StompAllocator : public AllocatorBase {
 public:
-    StompAllocator();
+    StompAllocator(bool checkOverrun);
     ~StompAllocator();
 
 private:
     virtual void* alloc_internal(size_t size);
     virtual void dealloc_internal(void *p);
+
+
+private:
+    using BlockSize = size_t;
+    static const size_t M_PAGE_SIZE;
+    const bool M_CHECK_OVERRUN;
 };
 
 #endif
