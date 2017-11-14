4c46675262ae15e8f7b91a8a58989abb7786bd48
Mikael Hernvall <mikael.hernvall@gmail.com>
2017-11-14 21:04:29 +0100

    Add STOMP intercept code to AllocatorBase

--- AllocatorBase.cpp
+++ AllocatorBase.cpp
@@ -1,8 +1,30 @@
 #include "AllocatorBase.h"
 
+#ifdef ENABLE_STOMP
+#include "StompAllocator.h"
+#endif // ENABLE_STOMP
+
 AllocatorBase::AllocatorBase() {
     std::cout << "ctor AllocatorBase" << std::endl;
+
+    #ifdef ENABLE_STOMP
+    mStompAllocator = new StompAllocator(this, true);
+    #endif // ENABLE_STOMP
 }
+
+#ifdef ENABLE_STOMP
+AllocatorBase::AllocatorBase(StompFlag stompFlag) {
+    std::cout << "ctor AllocatorBase" << std::endl;
+    mStompAllocator = this;
+}
+#endif // ENABLE_STOMP
+
 AllocatorBase::~AllocatorBase() {
     std::cout << "dtor AllocatorBase" << std::endl;
+
+    #ifdef ENABLE_STOMP
+    if(mStompAllocator != this) {
+        delete mStompAllocator;
+    }
+    #endif // ENABLE_STOMP
 }
